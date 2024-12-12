f90e659108a6c8430b25520cbde9f5a68b7b414d
Febrian SF <febriansf@student.uns.ac.id>
2024-12-12 08:51:26 +0700

    Add Search and Recommendation Feature

--- books.py
+++ books.py
@@ -18,6 +18,43 @@ def homePage():
                                 book_author=list(shuffled_books['Book-Author'].values),
                                 book_cover=list(shuffled_books['Image-URL-M'].values),
                                 )
+    
+def get_recommendation(title):
+    index = np.where(pivot_table.index == title[0])[0][0]
+    sim_score = sorted(list(enumerate(similarity_scores[index])), key=lambda x: x[1], reverse=True)
 
+    return books.iloc[[i[0] for i in sim_score]][['Book-Title', 'Book-Author', 'Image-URL-M']].copy()
+
+@app.route('/search', methods=['post'])
+def searchPage():
+    # Get User Input
+    user_input = request.form.get('user-input')
+    
+    # Search through book's data
+    starts_with = books[books['Book-Title'].str.startswith(user_input, na=False)][:6]
+    contains = books[books['Book-Title'].str.contains(user_input, case=False, na=False) & 
+                ~books['Book-Title'].str.startswith(user_input, na=False)][:12]
+
+    search_result = pd.concat([starts_with, contains]).drop_duplicates(subset='Book-Title')
+    
+    if user_input == '' or search_result.empty:
+        return render_template('search_result.html',
+                                data=['Not Found'])
+
+    elif search_result.iloc[0]['Book-Title'] in pivot_table.index.values:
+        recommendations = get_recommendation(search_result.iloc[0]['Book-Title'])
+    
+    else:
+        recommendations = popular_books[popular_books['Avg-Ratings'] > 4].sample(frac=1)
+    
+    return render_template('search_result.html',
+                                book_title=list(search_result['Book-Title'].values),
+                                book_author=list(search_result['Book-Author'].values),
+                                book_cover=list(search_result['Image-URL-M'].values),
+                                recomend_title=list(recommendations['Book-Title'].values),
+                                recomend_author=list(recommendations['Book-Author'].values),
+                                recomend_cover=list(recommendations['Image-URL-M'].values),
+                                )
+    
 if __name__ == '__main__':
     app.run(debug=True)
